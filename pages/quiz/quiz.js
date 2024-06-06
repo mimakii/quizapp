2630eab5ca18af9d05318d019dd4f038e997f433
Daniel Mimaki 21ATI <danielmimaki@gmail.com>
2024-06-06 22:01:12 +0000

    adicionada a função de validar a resposta

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -10,6 +10,7 @@ let pontos = 0
 let pergunta = 1
 let resposta = ""
 let idInputResposta = ""
+let respostaCorretaId = ""
 
 
 
@@ -132,7 +133,26 @@ function guardarResposta(evento){
     idInputResposta = evento.target.id
 
 
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.addEventListener("click", validarResposta)
+
+
 }
+
+
+
+function validarResposta() {
+    if (resposta === quiz.questions[pergunta-1].answer) {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+        pontos = pontos + 1
+    } else {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+    }
+}
+
+
+
 
 async function iniciar() {
     alterarAssunto()
@@ -143,6 +163,11 @@ async function iniciar() {
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         input.addEventListener("click", guardarResposta)
+
+
+        if (input.value === quiz.questions[pergunta-1].answer){
+            respostaCorretaId = input.id
+        }
     })
 }
 
