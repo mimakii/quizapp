d41d6e9cfc39fbea024a92555abfd98fc357b2f9
Daniel Mimaki 21ATI <danielmimaki@gmail.com>
2024-06-06 22:27:50 +0000

    adicionada as funçoes de proxima pergunta e finalizar o quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -116,7 +116,7 @@ function montarPergunta(){
             </label>
         </form>
 
-        <button>Enviar</button>
+        <button>Responder</button>
 
     </section>
     
@@ -142,6 +142,21 @@ function guardarResposta(evento){
 
 
 function validarResposta() {
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Próxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+    
+
+
+    if (pergunta === 10){
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
+
+
     if (resposta === quiz.questions[pergunta-1].answer) {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
         pontos = pontos + 1
@@ -149,17 +164,26 @@ function validarResposta() {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
         document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
     }
+
+    pergunta = pergunta + 1
+    console.log(pergunta)
+}
+
+
+function finalizar(){
+    localStorage.setItem("pontos", pontos)
+
+    window.location.href = "../resultado/resultado.html"
 }
 
 
 
-
-async function iniciar() {
-    alterarAssunto()
-   await buscarPerguntas()
+function proximaPergunta() {
     montarPergunta()
+    adicionarEventoInputs()
+}
 
-
+function adicionarEventoInputs(){
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input => {
         input.addEventListener("click", guardarResposta)
@@ -169,6 +193,16 @@ async function iniciar() {
             respostaCorretaId = input.id
         }
     })
+}
+
+async function iniciar() {
+    alterarAssunto()
+   await buscarPerguntas()
+    montarPergunta()
+    adicionarEventoInputs()
+
+
+    
 }
 
 iniciar()
